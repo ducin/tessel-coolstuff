60bf9061ab110f6ebfb7449dd4e4c04fa688eccd
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-07-14 18:20:07 +0200

    improving coolstuff

--- coolstuff.js
+++ coolstuff.js
@@ -17,68 +17,43 @@ ambient.on('error', function (err) {
 });
 
 
-var LIGHT_LEVEL = 0.4;
-var DARK_LEVEL = 0.2;
-
-function isLight(value) {
-  return value > LIGHT_LEVEL;
-}
-function isDark(value) {
-  return value < DARK_LEVEL;
-}
-
-function clear() {
-  tessel.led[0].on();
-  tessel.led[1].off();
-  tessel.led[2].off();
-  tessel.led[3].off();
-}
-
 // an array of available LEDs
 var leds = tessel.led;
 
-// ERR - Red
 var red = leds[0];
-
-// WLAN - Amber
 var amber = leds[1];
-
-// LED0 - Green
 var green = leds[2];
-
-// LED1 - Blue
 var blue = leds[3];
 
-/////////////////////////////////////////////
+function clear() {
+  red.off();
+  amber.off();
+  green.off();
+  blue.on();
+}
 
-//var ws = require("nodejs-websocket");
-//
-//// Scream server example: "hi" -> "HI!!!"
-//var server = ws.createServer(function (conn) {
-//  console.log("New connection");
-//  //conn.on("text", function (str) {
-//  //  console.log("Received "+str);
-//  //  conn.sendText(str.toUpperCase()+"!!!")
-//  //});
-//  ws.sendSound = function(value) {
-//    conn.send(value+"!!!");
-//    console.log("sent"+value);
-//  };
-//  conn.on("close", function (code, reason) {
-//    console.log("Connection closed")
-//  })
-//}).listen(8001);
+var handler;
+function blinkStart() {
+  red.on();
+  amber.off();
+  green.on();
+  blue.off();
+  handler = setInterval(function () {
+    red.toggle();
+    amber.toggle();
+    green.toggle();
+    blue.toggle();
+  }, 500);
+}
+
+function blinkEnd() {
+  clearInterval(handler);
+}
 
 var path = require('path');
 var av = require('tessel-av');
 var mp3 = path.join(__dirname, 'silence.mp3');
 var speaker = new av.Speaker(mp3);
-
-//speaker.play();
-//
-//speaker.on('end', function(seconds) {
-//  speaker.play();
-//});
 
 ////////////////////////////////////////
 
@@ -89,25 +64,22 @@ var NOISE_LEVEL = 0.09;
 var LOUD_LEVEL = 0.12;
 
 var isPlaying = false;
-speaker.on('end', function (seconds) {
+speaker.on('ended', function (seconds) {
+  console.log('I killed you');
   isPlaying = false;
   clear();
+  blinkEnd();
 });
 
 ambient.on('ready', function () {
   // Get points of light and sound data.
   setInterval( function () {
-    //ambient.getLightLevel( function(err, lightdata) {
-    //  if (err) throw err;
       ambient.getSoundLevel( function(err, sounddata) {
         if (err) throw err;
-        //var light = lightdata.toFixed(8);
         var sound = sounddata.toFixed(8);
         console.log(
-          //"Light level:", light, " ",
           "Sound Level:", sound
         );
-        //ws.sendSound(sound);
         if (!isPlaying) {
           if (sound < QUIET_LEVEL) {
             blue.on();
@@ -125,29 +97,16 @@ ambient.on('ready', function () {
             amber.on();
             red.off();
           } else {
-            blue.on();
-            green.on();
-            amber.on();
-            red.on();
-
+            //blue.on();
+            //green.on();
+            //amber.on();
+            //red.on();
             console.log("Silence I kill U");
             speaker.play();
+            blinkStart();
             isPlaying = true;
           }
         }
-
-        //if (light > LIGHT_LEVEL) {
-        //  tessel.led[2].on();
-        //} else if (light < DARK_LEVEL) {
-        //  tessel.led[2].off();
-        //}
       });
-    //});
-  }, 200); // The readings will happen every .5 seconds
+  }, 200);
 });
-
-//setInterval(function () {
-  //tessel.led[1].toggle();
-  //tessel.led[2].toggle();
-  //tessel.led[3].toggle();
-//}, 300);
